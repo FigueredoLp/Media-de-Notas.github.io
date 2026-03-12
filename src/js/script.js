e918532428412d63bee6adfa62fc74b94b3bf605
FigueredoLp <viviny.99@gmail.com>
2026-03-11 21:14:14 -0300

    Calculadora de Média

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,55 @@
 const botao = document.getElementById("botao");
+const resultado = document.getElementById("resultado");
+
+const nomeInput = document.getElementById("nome");
+const nota1Input = document.getElementById("nota1");
+const nota2Input = document.getElementById("nota2");
+const nota3Input = document.getElementById("nota3");
+const nota4Input = document.getElementById("nota4");
+
+let alunos = [];
 
 botao.addEventListener("click", function () {
-  let nome = document.getElementById("nome").value;
+  let nome = nomeInput.value;
 
-  let nota1 = Number(document.getElementById("nota1").value);
-  let nota2 = Number(document.getElementById("nota2").value);
-  let nota3 = Number(document.getElementById("nota3").value);
-  let nota4 = Number(document.getElementById("nota4").value);
-  console.log(nome, nota1, nota2, nota3, nota4);
+  let notas = [
+    Number(nota1Input.value),
+    Number(nota2Input.value),
+    Number(nota3Input.value),
+    Number(nota4Input.value),
+  ];
 
-  let media = nota1 + nota2;
-  console.log(media);
+  let media = notas[0] * 0.2 + notas[1] * 0.3 + notas[2] * 0.2 + notas[3] * 0.3;
+
+  let aluno = {
+    nome: nome,
+    notas: notas,
+    media: media.toFixed(2),
+  };
+
+  alunos.push(aluno);
+
+  mostrarAlunos();
+
+  nomeInput.value = "";
+  nota1Input.value = "";
+  nota2Input.value = "";
+  nota3Input.value = "";
+  nota4Input.value = "";
+
+  nomeInput.focus();
 });
+
+function mostrarAlunos() {
+  resultado.innerHTML = "";
+
+  alunos.forEach(function (aluno) {
+    resultado.innerHTML += `
+      <p>
+        <strong>${aluno.nome}</strong> |
+        Notas: ${aluno.notas.join(", ")} |
+        Média: ${aluno.media}
+      </p>
+    `;
+  });
+}
